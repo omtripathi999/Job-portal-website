8529ef25cbf999379254447d022bfb3cd55a63c6
Om Tripathi <69376724+omtripathi999@users.noreply.github.com>
2023-06-25 02:19:42 +0530

    Add files via upload

---
+++ About.php
@@ -0,0 +1,205 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+					<img src="https://webfeb.in/wp-content/uploads/2016/09/job-portal-1.jpg" alt="showcase image">
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<h3>DRIVEN BY PASSION, INNOVATION & YOU</h3>
+						<p>JobIN is one of the thought leaders in the fresher recruitment space in India. At JobIN, it has been our constant endeavour to assist candidates, land their first dream job opportunity and help companies on the other hand, hire the right fresh talent. In short we bridge the gap between employable talent and opportunity for freshers.</p>
+						 <p>We achieve it by making things simple yet more advanced, business-focused yet user-friendly, and striving to be agile yet reach the masses. The successful conversion of our idea into action has helped us become one of the leading giants in fresher hiring and inspires us to move Forward.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+				
+						
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>Why Choose Us?</span></h3>
+								</div>
+								<p>We can conclude that the JobIN(online job portal) will be a project with a high
+success rate which will be able to deliver all the needs and requirements of employee
+(job seekers) as well as the employer (recruiter). Job seekers can easily find jobs
+completely related to their field and put an application online and confirm if they are
+feasible for the job and they are hired by the employers or not.</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Solution</span></h3>
+								</div>		
+								<!-- Accordion starts -->
+								<div class="panel-group" id="accordion-alt3">
+								 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+								  <div class="panel">	
+									<!-- Panel heading -->
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 1
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseOne-alt3" class="panel-collapse collapse">
+										<!-- Panel body -->
+										<div class="panel-body">
+										  Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 2
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseTwo-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 3
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseThree-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 4
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseFour-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+										</div>
+									 </div>
+								  </div>
+								</div>
+								<!-- Accordion ends -->
+								
+							</div>
+							
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Expertise</span></h3>
+								</div>								
+								<h6>Web Development</h6>
+								<div class="progress pb-sm">
+								  <!-- White color (progress-bar-white) -->
+								  <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Designing</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>User Experience</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Development</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+							</div>
+							
+						</div>
+						
+						 						
+						 
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team1.jpg" alt="">
+										<!-- Name -->
+										<h4>Vaibhav</h4>
+										<span class="deg">Creative</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team2.jpg" alt="">
+										<!-- Name -->
+										<h4>Om Tripathi</h4>
+										<span class="deg">Programmer</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team3.jpg" alt="">
+										<!-- Name -->
+										<h4>Om Tripathi</h4>
+										<span class="deg">CEO</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>Vaibhav Mishra</h4>
+										<span class="deg">Manager</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
---
+++ home.php
@@ -0,0 +1,234 @@
+  <section id="banner">
+   
+  <!-- Slider -->
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/1.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>innovation</h3> 
+          <p>We create the opportunities</p> 
+           
+                </div>
+              </li>
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/2.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>Specialize</h3> 
+          <p>Success depends on work</p> 
+           
+                </div>
+              </li>
+            </ul>
+        </div>
+  <!-- end slider -->
+ 
+  </section> 
+  <section id="call-to-action-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-sm-9">
+          <h3>Partner with Business Leaders</h3>
+          <p>Development of successful, long term, strategic relationships between customers and suppliers, based on achieving best practice and sustainable competitive advantage. In the business partner model, HR professionals work closely with business leaders and line managers to achieve shared organisational objectives.</p>
+        </div>
+       <!--  <div class="col-md-2 col-sm-3">
+          <a href="#" class="btn btn-primary">Read More</a>
+        </div> -->
+      </div>
+    </div>
+  </section>
+  
+  <section id="content">
+  
+  
+  <div class="container">
+        <div class="row">
+      <div class="col-md-12">
+        <div class="aligncenter"><h2 class="aligncenter">Company</h2><!-- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus ovident, doloribus omnis minus temporibus perferendis nesciunt.. --></div>
+        <br/>
+      </div>
+    </div>
+
+    <?php 
+      $sql = "SELECT * FROM `tblcompany`";
+      $mydb->setQuery($sql);
+      $comp = $mydb->loadResultList();
+
+
+      foreach ($comp as $company ) {
+        # code...
+    
+    ?>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-building-o"></i>
+                <div class="info-blocks-in">
+                    <h3><?php echo $company->COMPANYNAME;?></h3>
+                    <!-- <p><?php echo $company->COMPANYMISSION;?></p> -->
+                    <p>Address :<?php echo $company->COMPANYADDRESS;?></p>
+                    <p>Contact No. :<?php echo $company->COMPANYCONTACTNO;?></p>
+                </div>
+            </div>
+
+    <?php } ?> 
+  </div>
+  </section>
+  
+  <section class="section-padding gray-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-title text-center">
+            <h2 >Popular Jobs</h2>  
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 ">
+          <?php 
+            $sql = "SELECT * FROM `tblcategory`";
+            $mydb->setQuery($sql);
+            $cur = $mydb->loadResultList();
+
+            foreach ($cur as $result) {
+              echo '<div class="col-md-3" style="font-size:15px;padding:5px">* <a href="'.web_root.'index.php?q=category&search='.$result->CATEGORY.'">'.$result->CATEGORY.'</a></div>';
+            }
+
+          ?>
+        </div>
+      </div>
+ 
+    </div>
+  </section>    
+  <section id="content-3-10" class="content-block data-section nopad content-3-10">
+  <div class="image-container col-sm-6 col-xs-12 pull-left">
+    <div class="background-image-holder">
+
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-6 col-xs-12 content">
+        <div class="editContent">
+          <h3>Our Team</h3>
+        </div>
+        <div class="editContent"  style="height:235px;">
+          <p> 
+          &nbsp;&nbsp;Our "one team" attitude breaks down silos and helps us engage equally effectively from the C-suite to the front line. Our collaborative working style emphasizes teamwork, trust, and tolerance for diverging opinions. People tell us we are down-to-earth, approachable and fun.<br/><br/>
+
+          &nbsp;&nbsp;We have a passion for our clients' true results and a pragmatic drive for action that starts Monday morning 8am and doesn't let up. We rally clients with our infectious energy, to make change stick.<br/><br/>
+
+          &nbsp;&nbsp;And we never go it alone. We support and are supported to develop our own personal results stories. We balance challenging and co-creating with our clients, building the internal capabilities required for them to create repeatable results. </p>
+        </div> 
+      </div>
+    </div><!-- /.row-->
+  </div><!-- /.container -->
+</section>
+  
+  <div class="about home-about">
+        <div class="container">
+            
+            <div class="row">
+              <div class="col-md-4">
+                <!-- Heading and para -->
+                <div class="block-heading-two">
+                  <h3><span>Programes</span></h3>
+                </div>
+                <p>The No. 1 job portal for freshmen in India is Job.in (A TeamLease Company), which hosts over 1.5 million resumes and has over 60 thousand recruiters using us to fill positions. Register today to apply for Top MNC Jobs and Government Jobs! What else? Receive daily job alerts via email. There is only one website, Job.in (A TeamLease Company), for applying to top MNC jobs and government jobs throughout India. Register right away to take advantage of our limitless fresher-focused services!</p>
+              </div>
+              <div class="col-md-4">
+                <div class="block-heading-two">
+                  <h3><span>Latest News</span></h3>
+                </div>    
+                <!-- Accordion starts -->
+                <div class="panel-group" id="accordion-alt3">
+                 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+                  <div class="panel"> 
+                  <!-- Panel heading -->
+                   <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+                      <i class="fa fa-angle-right"></i> 5 common questions for freshers
+                      </a>
+                    </h4>
+                   </div>
+                   <div id="collapseOne-alt3" class="panel-collapse collapse">
+                    <!-- Panel body -->
+                    <div class="panel-body">
+                     Tell me about yourself? What are you interested in this job? What would you say are your greatest strengths? What do you think are your biggest weaknesses? Where do you see yourself in five years?
+                    </div>
+                   </div>
+                  </div>
+                  <div class="panel">
+                   <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+                      <i class="fa fa-angle-right"></i> How do you keep technology skills?
+                      </a>
+                    </h4>
+                   </div>
+                   <div id="collapseTwo-alt3" class="panel-collapse collapse">
+                    <div class="panel-body">
+                    Tech professionals work hard to keep their knowledge base current by reading blogs and forums, taking online courses, joining hackathons and plugging away at personal IT projects.
+                    </div>
+                   </div>
+                  </div>
+                  <div class="panel">
+                   <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+                      <i class="fa fa-angle-right"></i> How do you handle tight deadlines?
+                      </a>
+                    </h4>
+                   </div>
+                   <div id="collapseThree-alt3" class="panel-collapse collapse">
+                    <div class="panel-body">
+                    IT teams often face daunting time constraints.
+                    </div>
+                   </div>
+                  </div>
+                  <div class="panel">
+                   <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+                      <i class="fa fa-angle-right"></i> Why do you want to work for us?
+                      </a>
+                    </h4>
+                   </div>
+                   <div id="collapseFour-alt3" class="panel-collapse collapse">
+                    <div class="panel-body">
+                    Individuals who truly did their homework will have done their research and be able to talk about your company’s values, products and services, and approach to technology.
+                   </div>
+                  </div>
+                </div>
+                <!-- Accordion ends -->
+                
+              </div>
+              
+              <div class="col-md-4">
+                <div class="block-heading-two">
+                  <h3><span>Testimonials</span></h3>
+                </div>  
+                     <div class="testimonials">
+                    <div class="active item">
+                      <blockquote><p>While getting featured by news outlets can be difficult for your business to achieve, the payoff can result in thousands of new leads flocking to your website. If your company is lucky enough to obtain this type of publicity, feed its success by sharing any related content on your website, blog, and social media pages</p></blockquote>
+                      <div class="carousel-info">
+                      <img alt="" src="img/team4.jpg" class="pull-left">
+                      <div class="pull-left">
+                        <span class="testimonials-name">Om Tripathi</span>
+                        <span class="testimonials-post">Technical Director</span>
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              
+            </div>
+            
+                        
+             
+            <br>
+           
+            </div>
+            
+          </div>
